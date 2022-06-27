36f2034cc0cc6e7850cb9cee865813cf7f4e1c89
Siddharth <45942534+sid4861@users.noreply.github.com>
2022-06-27 16:51:35 +0530

    email on shipping page

--- src/pages/OrderPrint.js
+++ src/pages/OrderPrint.js
@@ -5,9 +5,10 @@ import { useState } from 'react';
 export default function OrderPrint() {
 
     const [address, setAddress] = useState("");
+    const [email, setEmail] = useState("");
 
     return (
-        <div className='min-h-screen bg-[#DCCFB4] flex flex-col items-center justify-center' >
+        <div className='min-h-screen bg-[#DCCFB4] flex flex-col items-center justify-around' >
 
             <Player
                 autoplay
@@ -19,12 +20,18 @@ export default function OrderPrint() {
 
             <form onSubmit={(e) => {
                 e.preventDefault();
-                console.log(address);
             }} >
-                <p className='text-[18px] text-[#1E392D]' ><label for="address">Shipping address</label></p>
-                <textarea value={address} onChange={(e) => setAddress(e.target.value)} id="address" name="address" rows="4" cols="40" className="text-[#1E392D] mt-4 p-4 rounded-md outline-0 focus:outline-[#2F5B48] focus:outline focus:outline-2" ></textarea>
+                <div>
+                    <p className='text-[18px] text-[#1E392D]' ><label for="address">Shipping address</label></p>
+                    <textarea value={address} onChange={(e) => setAddress(e.target.value)} id="address" name="address" rows="4" cols="40" className="text-[#1E392D] mt-3 p-4 rounded-md outline-0 focus:outline-[#2F5B48] focus:outline focus:outline-2" ></textarea>
+                </div>
+                <div className='mt-8' >
+                    <p className='text-[18px] text-[#1E392D]' ><label for="email">Email</label></p>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" name="email" className="w-[100%] text-[#1E392D] mt-3 p-4 rounded-md outline-0 focus:outline-[#2F5B48] focus:outline focus:outline-2" />
+                </div>
 
-                <button type="submit" className='block mt-16 md:mt-8 px-32 py-4 bg-[#2F5B48] text-[#DCCFB4] shadow-[-6px_6px_0px_#CC6957]' >
+
+                <button type="submit" className='block mt-16 md:mt-8 px-36 py-4 bg-[#2F5B48] text-[#DCCFB4] shadow-[-6px_6px_0px_#CC6957]' >
                     <p className='animate-bounce' >Get it shipped</p>
                 </button>
             </form>
